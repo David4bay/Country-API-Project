49a752b8762531ed4b76c8f9e7fca09f6d72ada9
David4bay <david4bay@gmail.com>
2023-06-23 22:54:31 +0100

    updated layout

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
+
 function fetchData() {
-    return fetch('country.json').then(function (response) { return response.json(); })
-        .then(function (data) { return console.log(data); }).catch(function (error) {
+    return fetch('country.json')
+    .then(function (response) { 
+        return response.json(); 
+    })
+    .then(function (data) { 
+        return console.log(data); 
+    }
+    ).catch(function (error) {
         console.error('Error:', error);
     });
 }
-var loadData = fetchData();
-load;
+// test the country.json first
+fetchData();
