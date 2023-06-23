7554873de04eccfcf6f74fa951f1e7e7acbb4afc
David4bay <david4bay@gmail.com>
2023-06-23 21:29:14 +0100

    removed script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function fetchData() {
-    return fetch('country.json').then(function (response) { return response.json(); })
-        .then(function (data) { return console.log(data); }).catch(function (error) {
-        console.error('Error:', error);
-    });
-}
-var loadData = fetchData();
-load;
